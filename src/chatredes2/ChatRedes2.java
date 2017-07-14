7113db4d672163c25af1ea5ebfd230e12d9844d7
nailtonalves <nailtonalves@gmail.com>
2017-07-14 04:44:52 -0300

    Inicio do Projeto

---
+++ src/chatredes2/ChatRedes2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatredes2;
+
+/**
+ *
+ * @author Nailton
+ */
+public class ChatRedes2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaChat cliente = new TelaChat();
+        TelaIdCliente id = new TelaIdCliente();
+        id.setVisible(true);
+        cliente.setVisible(true);
+
+    }
+    
+}
